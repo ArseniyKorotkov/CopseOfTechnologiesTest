4b4a40aba5f8a0e3093d02a87a26e0c9ab427cfa
ArseniyKorotkov <69350678+ArseniyKorotkov@users.noreply.github.com>
2024-08-03 00:36:37 +0200

    fixed AngleAnalyzer logic corrected field names in AngleAnalyzer

--- TechTest/src/main/java/by/arsy/techtest/AngleAnalyzer.java
+++ TechTest/src/main/java/by/arsy/techtest/AngleAnalyzer.java
@@ -1,50 +1,56 @@
 package by.arsy.techtest;
 
+
 public class AngleAnalyzer {
 
     private static final double ROUND_ANGLE = 360;
     private static final int CORRECT_Y_UP_IS_GROW = -1;
 
-    private final double angle;
     private final double pixels;
     private final double refreshRate;
-
-    private double xValue = -1;
-    private double yValue = -1;
+    private final double angleToRadians;
+    private double xValue = Double.NaN;
+    private double yValue = Double.NaN;
 
 
     public AngleAnalyzer(double angle, double pixels, double refreshRate) {
-        this.angle = angle;
         this.pixels = pixels;
         this.refreshRate = refreshRate;
+        angleToRadians = angleToRadians(angle);
     }
 
+
     public double getX() {
-        if (xValue < 0) {
-            double angelToRadians = angleToRadians(angle);
-            xValue = pixels * Math.sin(angelToRadians) * refreshRate;
+        if (Double.isNaN(xValue)) {
+            xValue = pixels * Math.sin(angleToRadians) * refreshRate;
         }
         return xValue;
     }
 
+
     public double getY() {
-        if(yValue < 0) {
-            double angelToRadians = angleToRadians(angle);
-            yValue = pixels * Math.cos(angelToRadians) * refreshRate * CORRECT_Y_UP_IS_GROW;
+        if (Double.isNaN(yValue)) {
+            yValue = pixels * Math.cos(angleToRadians) * refreshRate * CORRECT_Y_UP_IS_GROW;
         }
         return yValue;
     }
 
+
     private double angleToRadians(double angle) {
+
         angle = fixAngle(angle);
-        return Math.toRadians(fixAngle(angle));
+        return Math.toRadians(angle);
     }
 
+
     private double fixAngle(double angle) {
+
         angle = angle % ROUND_ANGLE;
+
         if (angle < 0) {
             angle = ROUND_ANGLE - angle;
         }
+
         return angle;
     }
 }
